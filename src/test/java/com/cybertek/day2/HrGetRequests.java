18f4a1dbfe22e0aaaf7aa0d2477004bfddf3a1d4
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-16 08:46:18 +0200

    Task added to HR

--- src/test/java/com/cybertek/day2/HrGetRequests.java
+++ src/test/java/com/cybertek/day2/HrGetRequests.java
@@ -1,6 +1,10 @@
 package com.cybertek.day2;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
 
@@ -10,6 +14,28 @@ public class HrGetRequests {
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://54.91.11.180:1000/ords/hr";
+        RestAssured.baseURI = "http://54.91.11.180:1000/ords/hr";
     }
+
+
+    @DisplayName("GET request to /regions")
+    @Test
+    public void test1(){
+
+       Response response = RestAssured.get("/regions");
+
+       //print the status code
+        System.out.println(response.statusCode());
+
+    }
+
+     /*
+        Given accept type is application/json
+        When user sends get request to /regions/2
+        Then response status code must be 200
+        and content type equals to application/json
+        and response body contains   Americas
+     */
+
+
 }
