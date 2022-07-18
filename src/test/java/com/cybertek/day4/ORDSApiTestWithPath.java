9b0fda60a9a31d00b45aaa3487d47f0ad102e8d2
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-18 23:38:15 +0200

    Day4: SpartanWithJsonPath added

--- src/test/java/com/cybertek/day4/ORDSApiTestWithPath.java
+++ src/test/java/com/cybertek/day4/ORDSApiTestWithPath.java
@@ -15,15 +15,15 @@ public class ORDSApiTestWithPath extends HRTestBase {
 
     @DisplayName("GET request to countries with Path method")
     @Test
-    public void test1(){
+    public void test1() {
 
-        Response response= given().accept(ContentType.JSON)
-                .queryParam("q","{\"region_id\":2}")
+        Response response = given().accept(ContentType.JSON)
+                .queryParam("q", "{\"region_id\":2}")
                 .when()
                 .get("/countries");
 
-        assertEquals(200,response.statusCode());
-        
+        assertEquals(200, response.statusCode());
+
         //print limit result
         System.out.println("response.path(\"limit\") = " + response.path("limit"));
 
@@ -31,7 +31,7 @@ public class ORDSApiTestWithPath extends HRTestBase {
         System.out.println("response.path(\"hasMore\") = " + response.path("hasMore"));
 
         //print first countryId
-     String firstCountryId = response.path("items[0].country_id");
+        String firstCountryId = response.path("items[0].country_id");
         System.out.println("firstCountryId = " + firstCountryId);
 
         //print second country name
@@ -51,8 +51,32 @@ public class ORDSApiTestWithPath extends HRTestBase {
 
         for (Integer regionsID : allRegionsIDs) {
             System.out.println("regionsID = " + regionsID);
-            assertEquals(2,regionsID);
+            assertEquals(2, regionsID);
         }
+
+    }
+
+        @DisplayName("GET request to /employees with Query Param")
+        @Test
+        public void test2(){
+            Response response= given().accept(ContentType.JSON)
+                    .and().queryParam("q","{\"job_id\": \"IT_PROG\"}")
+                    .when().get("/employees");
+
+            assertEquals(200,response.statusCode());
+            assertEquals("application/json",response.header("Content-Type"));
+            assertTrue(response.body().asString().contains("IT_PROG"));
+
+            //make sure we have only IT_PROG as a job_id
+            List<String> allJobIDs = response.path("items.job_id");
+
+            for (String jobID : allJobIDs) {
+                System.out.println("jobID = " + jobID);
+                assertEquals("IT_PROG",jobID);
+            }
+
+            //HW
+            //print name of each IT_PROGs
 
 
     }
