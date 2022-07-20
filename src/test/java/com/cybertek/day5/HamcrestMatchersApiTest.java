a62f05f714a2593ab5baf79a3fbcbf2677b95362
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-20 21:53:16 +0200

    Day5: HamcrestMatchersApiTest.java first test case done

--- src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -1,5 +1,6 @@
 package com.cybertek.day5;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,20 @@ public class HamcrestMatchersApiTest {
     @Test
     public void test1(){
 
+        given().log().all().
+                accept(ContentType.JSON)
+                .and().pathParam("id",15)
+        .when()
+                .get("http://52.207.61.129:8000/api/spartans/{id}")
+        .then()
+                .statusCode(200)
+                .and().assertThat()
+                .contentType("application/json")
+                .and()
+                .body("id",equalTo(15),
+      "name",is("Meta"),
+                            "gender",is("Female"),
+                            "phone",is(1938695106))
+                .log().all();
     }
 }
