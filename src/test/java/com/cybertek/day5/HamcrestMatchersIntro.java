fcd7e95e00150480b51c1b4c49965643de0f5e35
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-20 14:08:25 +0200

    Day5: HamcrestMatcherIntro .java done

--- src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
@@ -2,14 +2,14 @@ package com.cybertek.day5;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
-
 
 public class HamcrestMatchersIntro {
 
@@ -17,12 +17,13 @@ public class HamcrestMatchersIntro {
     @Test
     public void simpleTest1(){
 
-        assertThat( 5+5, is(10));
+        //actual 5+5
+        assertThat(5+5, is(10));
         assertThat(5+5,equalTo(10));
         //matchers has 2 overloaded version
         //first that accept actual value
-        //second that accept another matchers
-        //below examples is method is accepting another matcher equal to make it readable
+        //second taht accept another matchers
+        //below examples is method is accepting another matchers equal to make it readable
         assertThat(5+5,is(equalTo(10)));
 
         assertThat(5+5,not(9));
@@ -32,19 +33,18 @@ public class HamcrestMatchersIntro {
         //number comparison
         //greaterThan()
         //greaterThanOrEqualTo()
-        //lessThan
-        //lessThanOrEqualTo
+        //lessThan()
+        //lessThanOrEqualTo()
         assertThat(5+5,is(greaterThan(9)));
-
     }
 
     @DisplayName("Assertion with String")
     @Test
-    public void StringHamcrest(){
+    public void stringHamcrest(){
 
         String text = "B22 is learning Hamcrest";
 
-        //checking for equality is same as number
+        //checking for euqality is same as numbers
         assertThat(text,is("B22 is learning Hamcrest"));
         assertThat(text,equalTo("B22 is learning Hamcrest"));
         assertThat(text,is(equalTo("B22 is learning Hamcrest")));
@@ -53,7 +53,7 @@ public class HamcrestMatchersIntro {
         assertThat(text,startsWith("B22"));
         //now do it in case insensitive manner
         assertThat(text,startsWithIgnoringCase("b22"));
-        //endsWith
+        //endswith
         assertThat(text,endsWith("rest"));
 
         //check if text contains String learning
@@ -61,13 +61,31 @@ public class HamcrestMatchersIntro {
         //with ignoring case
         assertThat(text,containsStringIgnoringCase("LEARNING"));
 
-        String str = " ";
+        String str ="  ";
+
         //check if above str is blank
         assertThat(str,blankString());
         //check if trimmed str is empty string
-        assertThat(str.trim(),emptyString())
+        assertThat(str.trim(),emptyString());
+    }
 
+    @DisplayName("Hamcrest for Collection")
+    @Test
+    public void testCollection(){
+
+        List<Integer> listOfNumbers = Arrays.asList(1,4,5,6,32,54,66,77,45,23);
+
+        //check size of the list
+        assertThat(listOfNumbers,hasSize(10));
+        //check if this list hasItem 77
+        assertThat(listOfNumbers,hasItem(77));
+        //check if this list hasItems 77,54,23
+        assertThat(listOfNumbers,hasItems(77,54,23));
+
+        //check if all numbers greater than 0
+        assertThat(listOfNumbers,everyItem(greaterThan(0)));
 
     }
+
 
 }
