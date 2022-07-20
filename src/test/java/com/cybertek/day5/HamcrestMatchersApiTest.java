03991de9c0a2318effd704151afff3004d21b76d
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-20 22:36:07 +0200

    Day5: HamcrestMatchersApiTest.java second test case done

--- src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -12,22 +12,50 @@ public class HamcrestMatchersApiTest {
 
     @DisplayName("OneSpartan with Hamcrest and chaining")
     @Test
-    public void test1(){
+    public void test1() {
 
         given().log().all().
                 accept(ContentType.JSON)
-                .and().pathParam("id",15)
-        .when()
+                .and().pathParam("id", 15)
+                .when()
                 .get("http://52.207.61.129:8000/api/spartans/{id}")
-        .then()
+                .then()
                 .statusCode(200)
                 .and().assertThat()
                 .contentType("application/json")
                 .and()
-                .body("id",equalTo(15),
-      "name",is("Meta"),
-                            "gender",is("Female"),
-                            "phone",is(1938695106))
+                .body("id", equalTo(15),
+                        "name", is("Meta"),
+                        "gender", is("Female"),
+                        "phone", is(1938695106))
                 .log().all();
     }
+
+    @DisplayName("CBTraining Teacher request with chaining and matchers")
+    @Test
+    public void teacherData() {
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .pathParam("id", 10423)
+                .and()
+
+                .when()
+                .get("http://api.cybertektraining.com/teacher/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json;charset=UTF-8")
+                .and()
+                .header("Content-Length", is("236"))
+                .and()
+                .header("Date", notNullValue())
+                .and().assertThat()
+                .body("teachers[0].firstName", is("Alexander"))
+                .body("teachers[0].lastName", is("Syrup"))
+                .body("teachers[0].gender", equalTo("male"));
+    }
+
+
 }
