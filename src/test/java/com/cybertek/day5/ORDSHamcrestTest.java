5a5c701ec6d0574cd3a5af406301ea3c4a84a160
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-21 00:00:07 +0200

    Day5: ORDSHamcrestTest.java added

--- src/test/java/com/cybertek/day5/ORDSHamcrestTest.java
+++ src/test/java/com/cybertek/day5/ORDSHamcrestTest.java
@@ -1,8 +1,16 @@
 package com.cybertek.day5;
 
 import com.cybertek.utilities.HRTestBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class ORDSHamcrestTest {
 
@@ -14,6 +22,20 @@ public class ORDSHamcrestTest {
         //verify first names are .... (find proper method to check list against list)
         //verify emails without checking order (provide emails in different order,just make sure it has same emails)
         //expected names
+        List<String> names = Arrays.asList("Alexander","Bruce","David","Valli","Diana");
+
+        given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"job_id\": \"IT_PROG\"}")
+        .when()
+                .get("/employees")
+        .then()
+                .statusCode(200)
+                .body("items.job_id",everyItem(equalTo("IT_PROG")))
+                .body("items.first_name",containsInRelativeOrder("Alexander","Bruce","David","Valli","Diana")) //contains with order
+                .body("items.email",containsInAnyOrder("VPATABAL","DAUSTIN","BERNST","AHUNOLD","DLORENTZ")) //contains without order
+                .body("items.first_name", equalTo(names)); // equality of lists assertion
+
+
     }
 
 }
