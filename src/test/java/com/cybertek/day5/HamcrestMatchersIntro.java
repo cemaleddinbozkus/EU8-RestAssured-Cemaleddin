18e8e2279a9f875ccf5dc04b8fa9d2af65c7e9f9
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-20 13:32:42 +0200

    Day5: HamcrestMatcherIntro .java string examples

--- src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
@@ -3,6 +3,7 @@ package com.cybertek.day5;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,6 +13,7 @@ import static org.hamcrest.Matchers.*;
 
 public class HamcrestMatchersIntro {
 
+    @DisplayName("Assertion with numbers")
     @Test
     public void simpleTest1(){
 
@@ -35,4 +37,37 @@ public class HamcrestMatchersIntro {
         assertThat(5+5,is(greaterThan(9)));
 
     }
+
+    @DisplayName("Assertion with String")
+    @Test
+    public void StringHamcrest(){
+
+        String text = "B22 is learning Hamcrest";
+
+        //checking for equality is same as number
+        assertThat(text,is("B22 is learning Hamcrest"));
+        assertThat(text,equalTo("B22 is learning Hamcrest"));
+        assertThat(text,is(equalTo("B22 is learning Hamcrest")));
+
+        //check if this text starts with B22
+        assertThat(text,startsWith("B22"));
+        //now do it in case insensitive manner
+        assertThat(text,startsWithIgnoringCase("b22"));
+        //endsWith
+        assertThat(text,endsWith("rest"));
+
+        //check if text contains String learning
+        assertThat(text,containsString("learning"));
+        //with ignoring case
+        assertThat(text,containsStringIgnoringCase("LEARNING"));
+
+        String str = " ";
+        //check if above str is blank
+        assertThat(str,blankString());
+        //check if trimmed str is empty string
+        assertThat(str.trim(),emptyString())
+
+
+    }
+
 }
