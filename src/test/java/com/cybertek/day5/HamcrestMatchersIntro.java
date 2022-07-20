d7d0e74c65e1966695a8da92192cd0a54b89de3f
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-20 13:04:05 +0200

    Day5: HamcrestMatcherIntro .java number

--- src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
@@ -1,5 +1,8 @@
 package com.cybertek.day5;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,5 +16,23 @@ public class HamcrestMatchersIntro {
     public void simpleTest1(){
 
         assertThat( 5+5, is(10));
+        assertThat(5+5,equalTo(10));
+        //matchers has 2 overloaded version
+        //first that accept actual value
+        //second that accept another matchers
+        //below examples is method is accepting another matcher equal to make it readable
+        assertThat(5+5,is(equalTo(10)));
+
+        assertThat(5+5,not(9));
+        assertThat(5+5,is(not(9)));
+        assertThat(5+5,is(not(equalTo(9))));
+
+        //number comparison
+        //greaterThan()
+        //greaterThanOrEqualTo()
+        //lessThan
+        //lessThanOrEqualTo
+        assertThat(5+5,is(greaterThan(9)));
+
     }
 }
