3ac202d77133d8761b95971ad35f26515cc67b04
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-20 12:08:20 +0200

    Day4: HamcrestMatcherIntro .java created

--- src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
@@ -1,4 +1,17 @@
 package com.cybertek.day5;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+
 public class HamcrestMatchersIntro {
+
+    @Test
+    public void simpleTest1(){
+
+        assertThat( 5+5, is(10));
+    }
 }
