42b9b5c0bf280ec2c0c563bd7966182483529cee
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-22 00:13:59 +0200

    Day5: JSON toJAVATest.java

---
+++ src/test/java/com/cybertek/day5/JSONtoJAVATest.java
@@ -0,0 +1,9 @@
+package com.cybertek.day5;
+
+import com.cybertek.utilities.SpartanTestBase;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class JSONtoJAVATest extends SpartanTestBase {
+}
