292398f6d9d100ea22f6087fcea89bb777c333c4
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-23 15:24:09 +0200

    Day6:Spartan.java first way added

--- src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
@@ -4,6 +4,7 @@ import com.cybertek.pojo.Spartan;
 import com.cybertek.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.internal.RestAssuredResponseOptionsGroovyImpl;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
@@ -15,7 +16,7 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
     @DisplayName("GET one spartan and convert it to Spartan Object")
     @Test
     public void oneSpartanPojo(){
-        given().accept(ContentType.JSON)
+        Response response = given().accept(ContentType.JSON)
                 .and().pathParam("id", 15)
                 .when().get("/api/spartans/{id}")
                 .then().statusCode(200)
