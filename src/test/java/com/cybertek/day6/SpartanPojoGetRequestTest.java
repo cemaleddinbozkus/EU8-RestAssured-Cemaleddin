c3d8d666989184122e43d65093bf0a9ec1605b71
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-23 15:14:12 +0200

    Day6:Spartan.java first way added

--- src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
@@ -1,6 +1,9 @@
 package com.cybertek.day6;
 
+import com.cybertek.pojo.Spartan;
 import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.internal.RestAssuredResponseOptionsGroovyImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
@@ -12,6 +15,22 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
     @DisplayName("GET one spartan and convert it to Spartan Object")
     @Test
     public void oneSpartanPojo(){
+        given().accept(ContentType.JSON)
+                .and().pathParam("id", 15)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200)
+                .extract().response();
+
+        //De serialize --> JSON to POJO (java custom class)
+        //2 different way to do this
+        //1.using as() method
+        //we convert json response to spartan object with the help of jackson
+        //as() method uses jackson to de serialize(converting JSON to Java class)
+       Spartan spartan15 = response.as(Spartan.class);
+        System.out.println(spartan15);
+        System.out.println("spartan15.getId() = " + spartan15.getId());
+        System.out.println("spartan15.getGender() = " + spartan15.getGender());
+
 
     }
 }
