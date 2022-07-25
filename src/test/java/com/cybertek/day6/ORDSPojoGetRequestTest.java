d5a2dc84619b7e0b8146d7b32a8735a12c663914
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-25 19:51:04 +0200

    Day6: ORDSPojoGetRequestTest.java added

---
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -0,0 +1,18 @@
+package com.cybertek.day6;
+
+import com.cybertek.utilities.HRTestBase;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ORDSPojoGetRequestTest extends HRTestBase {
+
+    @Test
+    public void regionTest(){
+
+
+    }
+
+}
