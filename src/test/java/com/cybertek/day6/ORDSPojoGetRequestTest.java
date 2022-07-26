52ccae796f0dacc7efd09954fdfebf2b426bb53f
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-26 18:48:22 +0200

    day6:regions class update with @JsonProperty annotation

--- src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -27,7 +27,7 @@ public class ORDSPojoGetRequestTest<Employee> extends HRTestBase {
 
         System.out.println(region1);
 
-        System.out.println("region1.getRegion_id() = " + region1.getRegion_id());
+        System.out.println("region1.getRegion_id() = " + region1.rId());
         System.out.println("region1.getRegion_name() = " + region1.getRegion_name());
         System.out.println("region1.getLinks().get(0).getHref() = " + region1.getLinks().get(0).getHref());
 
