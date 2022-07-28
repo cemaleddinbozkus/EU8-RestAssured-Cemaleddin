5a756d9adbec1cdf7c0c9d5cdb0dd29bd96b577e
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-28 13:30:33 +0200

    day6:regions task changed

--- src/test/java/com/cybertek/pojo/Region.java
+++ src/test/java/com/cybertek/pojo/Region.java
@@ -11,18 +11,14 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-@JsonIgnoreProperties(ignoreUnknown = true)
-
 public class Region {
     //region_id
     //if your jsonkey and variable name not matching, you can map it with jsonProperty
     @JsonProperty("region_id")
     private int rId;
     @JsonProperty("region_name")
-    private String region_name ;
-    //@JsonProperty("links")
-    //private List<Link> links;
-
-
+    private String region_name;
+    @JsonProperty("links")
+    private List<Link> links;
 
 }
