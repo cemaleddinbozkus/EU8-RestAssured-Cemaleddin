713fdba526518e6f1d84ad65e301356eba93d371
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-08-01 19:41:18 +0200

    day8:new SpartanAuthTestBase.java and SpartanWithAuthTests.java

--- src/test/java/com/cybertek/utilities/SpartanAuthTestBase.java
+++ src/test/java/com/cybertek/utilities/SpartanAuthTestBase.java
@@ -8,7 +8,7 @@ public abstract class SpartanAuthTestBase {
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://54.91.11.180:7000";
+        baseURI = "http://3.88.217.216:7000";
 
     }
 }
