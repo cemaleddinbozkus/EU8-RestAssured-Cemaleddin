2c3b2d53512377dcaa9b667269967267725a76f5
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-08-08 15:53:59 +0200

    day10:Json Schema validator updated

---
+++ src/test/java/com/cybertek/day10/JsonSchemaValidationTest.java
@@ -0,0 +1,19 @@
+package com.cybertek.day10;
+
+import com.cybertek.utilities.SpartanAuthTestBase;
+import com.fasterxml.jackson.core.json.PackageVersion;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class JsonSchemaValidationTest extends SpartanAuthTestBase {
+
+    @DisplayName("GET request to verify one spartan against to schema")
+    @Test
+    public void schemaValidation(){
+
+    }
+}
