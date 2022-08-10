b13c5f167c235f77c6ca535639a775218d355dac
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-08-10 10:07:46 +0200

    day10:ResponseTimeTest.java added

---
+++ src/test/java/com/cybertek/day10/ResponseTimeTest.java
@@ -0,0 +1,17 @@
+package com.cybertek.day10;
+
+import com.cybertek.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ResponseTimeTest {
+
+    @Test
+    public void test1(){
+
+
+    }
+}
